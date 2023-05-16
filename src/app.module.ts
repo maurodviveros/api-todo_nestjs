a29c95ac9a1fb1ef92a6fcd6d5ae912cebb9c2ef
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-15 21:45:00 -0300

    feat: :rocket: add GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppResolver } from "./app.resolver";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb+srv://courses:courses@courses.shrxl5n.mongodb.net/nestjs_todo",
+    ),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
